ff706df81f055380ac17ebba08e6f1c20e06cb30
Justin Barabander <jbarabander@gmail.com>
2015-11-11 02:02:59 -0500

    started working on nnls

--- index.js
+++ index.js
@@ -26,12 +26,21 @@ var math = require('mathjs');
 //basic NNLS algorithm
 
 function NNLS(coefficientMatrix, dependentVector) {
-	var coefficients = math.matrix(coefficientMatrix);
-	console.dir(coefficients);
+
 	if(coefficientMatrix.length !== dependentVector.length){
 		throw new Error('Coefficient Matrix and depent vector do not have same number of rows');	
 	}
+	var coefficients = math.matrix(coefficientMatrix);
 	var regressors = math.zeros(coefficients.size()[1]);
+	var passiveSet = [];
+	var activeSet = math.range(0, regressors.size()[0]);
+	var multipliers = math.multiply(math.transpose(coefficients), math.subtract(dependentVector, math.multiply(coefficients, regressors)));
+	console.log(multipliers);
 }
 
 NNLS([[1,2,5], [3,6,9]], [1,4]);
+
+//FNNLS algorithm
+function FNNLS(coefficientMatrix, dependentVector) {
+
+}
