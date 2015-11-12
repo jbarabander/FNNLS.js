3d5b06d0657a7c402a81c593481f312117576272
Justin Barabander <jbarabander@gmail.com>
2015-11-12 14:57:35 -0500

    Seems like algorithm is working but has no inner loop yet

--- index.js
+++ index.js
@@ -41,22 +41,24 @@ function NNLS(independentMatrix, dependentVector, tolerance) {
 		throw new Error('independent matrix and dependent vector do not have same number of rows');	
 	}
 	var coefficients = math.matrix(independentMatrix);
-	var regressors = math.zeros(coefficients.size()[1]);
+	var regressors = math.zeros(coefficients.size()[1]).valueOf();
 	var passiveSet = [];
-	var activeSet = arrayMath.range(0, regressors.size()[0]);
+	var activeSet = arrayMath.range(0, regressors.length);
 	var multipliers = math.multiply(math.transpose(coefficients), math.subtract(dependentVector, math.multiply(coefficients, regressors)));
 	var activeMultipliers = math.subset(multipliers, math.index(activeSet));
 	var maxActiveMultiplier = math.max(activeMultipliers);
-	var indexOfMax = activeMultipliers.valueOf().indexOf(maxActiveMultiplier);
-	var coefficientsP = math.matrix(math.zeros(coefficients.size()));
-	var columnRange = arrayMath.range(0, coefficients.size()[0]);
-	console.log(multipliers);
+	var indexOfMax = multipliers.valueOf().indexOf(maxActiveMultiplier);
+	var startingPassiveMatrix = math.matrix(math.zeros(coefficients.size()));
+	var rowRange = arrayMath.range(0, coefficients.size()[0]);
 
 	while(activeSet.length && maxActiveMultiplier > tolerance) {
-		passiveSet = passiveSet.concat(activeSet.splice(indexOfMax,1));
-		coefficientsP = coefficientsP.subset(math.index(columnRange, indexOfMax),coefficients.subset(math.index(columnRange, indexOfMax)));
+		// console.log(activeSet[activeSet.indexOf(indexOfMax)]);
+		passiveSet.push(activeSet.splice(activeSet.indexOf(indexOfMax),1)[0]);
+		var columnIndex = passiveSet.length === 1 ? passiveSet[0] : passiveSet;
+		var coefficientsP = startingPassiveMatrix.subset(math.index(rowRange, columnIndex),coefficients.subset(math.index(rowRange, columnIndex)));
 		var tCoefficientsP = math.transpose(coefficientsP);
 		var pseudoInv = pInv(coefficientsP.valueOf());
+		console.log('inverse check',numeric.dot(pseudoInv, coefficientsP.valueOf()));
 		// var pInv = numeric.dot(numeric.transpose(U), numeric.dot(numeric.diag(sInv), numeric.transpose(V)));
 		var sP = numeric.dot(numeric.dot(pseudoInv, tCoefficientsP.valueOf()), dependentVector);
 		while(math.min(sP.filter(function(element, index) {
@@ -71,13 +73,16 @@ function NNLS(independentMatrix, dependentVector, tolerance) {
 		}
 		regressors = sP;
 		multipliers = math.multiply(math.transpose(coefficients), math.subtract(dependentVector, math.multiply(coefficients, regressors)));
-		activeMultipliers = math.subset(multipliers, math.index(activeSet));
-		maxActiveMultiplier = math.max(activeMultipliers);
-		console.log('active', activeMultipliers);
-		indexOfMax = activeMultipliers.valueOf().indexOf(maxActiveMultiplier);
+		if(activeSet.length) {
+			activeMultipliers = math.subset(multipliers, math.index(activeSet));
+			maxActiveMultiplier = math.max(activeMultipliers);
+			// console.log('maxActiveMultiplier', maxActiveMultiplier);
+			indexOfMax = multipliers.valueOf().indexOf(maxActiveMultiplier);	
+		}
 		// console.log(multipliers);
-		console.log('regressors',regressors);
 	}
+	console.log('regressors',regressors);
+	return regressors;
 }
 
 function pInv(matrix) {
